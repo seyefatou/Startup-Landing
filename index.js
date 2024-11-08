268d8de9f8be2b80b607bb773ed92432d781937a
Fatou Seye <sidyseye1972@gmail.com>
2024-11-08 19:09:32 +0000

    amélioration projet gestion de responsive

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+
+
 function playVideo() {
-    var imageContainer = document.getElementById('image-container');
-    var videoContainer = document.getElementById('video-container');
-    var video = document.getElementById('video');
+  var imageContainer = document.getElementById('image-container');
+  var videoContainer = document.getElementById('video-container');
+  var video = document.getElementById('video');
 
 
-    imageContainer.style.opacity = 0;
+  imageContainer.style.opacity = 0;
 
 
-    setTimeout(function() {
-      imageContainer.style.display = 'none';
-      videoContainer.style.display = 'block';
-      video.play();
-    }, 300);
-  }
+  setTimeout(function() {
+    imageContainer.style.display = 'none';
+    videoContainer.style.display = 'block';
+    video.play();
+  }, 300);
+}
 
 // ___________carousel_________
 let currentIndex = 0;
@@ -21,15 +23,15 @@ const buttons = document.querySelectorAll('.buttons button');
 
 function showProfile(index) {
 
-    profiles[currentIndex].classList.remove('active');
-    buttons[currentIndex].classList.remove('active');
+  profiles[currentIndex].classList.remove('active');
+  buttons[currentIndex].classList.remove('active');
 
 
-    currentIndex = index;
+  currentIndex = index;
 
 
-    profiles[currentIndex].classList.add('active');
-    buttons[currentIndex].classList.add('active');
+  profiles[currentIndex].classList.add('active');
+  buttons[currentIndex].classList.add('active');
 }
 
 
@@ -38,7 +40,7 @@ buttons[currentIndex].classList.add('active');
 
 
 setInterval(() => {
-    showProfile((currentIndex + 1) % profiles.length);
+  showProfile((currentIndex + 1) % profiles.length);
 }, 3000);
 
 
